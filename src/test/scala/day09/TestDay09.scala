7db9bb74ba634afcc06c8650cf61fc5138a03a49
marinos <marinosch.99981@gmail.com>
2024-12-09 20:44:05 +0100

    day 9 solved but more debugging needed as bug was introduced with the free blocks map

--- src/test/scala/day09/TestDay09.scala
+++ src/test/scala/day09/TestDay09.scala
@@ -2,29 +2,57 @@ package org.mpdev.scala.aoc2024
 package day09
 
 import framework.AocMain
-import solutions.day09.Day09Solver
+import solutions.day09.DiskFragmenter
+import utils.also
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 
 class TestDay09 extends AnyFlatSpec {
 
-    AocMain.environment = "test"
-    private val solver = Day09Solver()
+    AocMain.environment = "prod"
+    private val solver = DiskFragmenter()
 
     it should "read input and setup list" in {
-        solver.inputData.foreach(println)
-        solver.inputData.size shouldBe 9
+        println(solver.filesSize)
+        println(solver.freeSpace)
+        println(solver.diskBlocks)
+        (solver.filesSize.size, solver.freeSpace.size,
+            solver.diskBlocks.map(i => if i == -1 then "." else i.toString).mkString) shouldBe
+            (10, 9, "00...111...2...333.44.5555.6666.777.888899")
+         //          012345678901234567890123456789012345678901
+         //          0         1         2         3         4
+    }
+
+    it should "defragment the disk - move individual blocks" in {
+        val defragmented = solver.defragmentDisk
+        println(defragmented)
+        defragmented.map(i => if i == -1 then "." else i.toString).mkString shouldBe "0099811188827773336446555566.............."
     }
 
     it should "solve part1 correctly" in {
-        val result = solver.part1
-        println(result)
-        result shouldBe 3749
+        val result = solver.part1.also(println)
+        result shouldBe 1928
+    }
+
+    it should "map files and free spaces" in {
+        val (files, freeMap, freeList) = solver.findFilesAndFreeBlocksIndexesAndSizes(solver.diskBlocks)
+        println(files)
+        println(s"freeMap: $freeMap")
+        println(s"freeList: $freeList")
+        println(solver.diskBlocks.map(i => if i == -1 then "." else i.toString).mkString)
+        println(files.size)
+        println(freeMap.size)
+        println(freeList.size)
+    }
+
+    it should "defragment the disk - move whole files" in {
+        val defragmented = solver.defragmentDisk2
+        println(defragmented)
+        defragmented.map(i => if i == -1 then "." else i.toString).mkString shouldBe "00992111777.44.333....5555.6666.....8888.."
     }
 
     it should "solve part2 correctly" in {
-        val result = solver.part2
-        println(result)
-        result shouldBe 11387
+        val result = solver.part2.also(println)
+        result shouldBe 2858
     }
 }
--- src/test/scala/grid/TestGrid.scala
+++ src/test/scala/grid/TestGrid.scala
@@ -7,15 +7,19 @@ import org.scalatest.matchers.should.Matchers.*
 
 class TestGrid extends AnyFlatSpec {
 
-    private val input = List(
+    private val inputString = List(
         "012",
         "345",
         "678"
     )
-    private val mapper = ('0' to '9').map( c => (c, c - '0')).toMap
+    private val mapperChar = ('0' to '9').map( c => (c, c - '0')).toMap
 
-    it should "read input and setup int grid" in {
-        val g = GridBuilder[Int]().withMapper(mapper).fromVisualGrid(input).withPrintFormat("%c, ").build()
+    it should "read input from List[String] and setup int grid" in {
+        val g = GridBuilder[Int]()
+            .withMapper(mapperChar)
+            .fromVisualGrid(inputString)
+            .withPrintFormat("%c, ")
+            .build()
         g.printIt()
     }
 }
