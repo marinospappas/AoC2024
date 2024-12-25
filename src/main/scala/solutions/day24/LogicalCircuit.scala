c15c6df373e5c4707bc4abeb1d115f6a1126b7c3
marinos <marinosch.99981@gmail.com>
2024-12-25 18:32:35 +0100

    day 24 manual solution by visualising the circuit and analysing in Exel. Need to write proper algorithm

--- src/main/scala/solutions/day24/LogicalCircuit.scala
+++ src/main/scala/solutions/day24/LogicalCircuit.scala
@@ -5,6 +5,11 @@ import framework.{InputReader, PuzzleSolver}
 import solutions.day24.LogicalCircuit.{readGate, readInput}
 import solutions.day24.Gate.{AND, OR, XOR}
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.util.boundary
+import scala.util.boundary.break
+
 class LogicalCircuit(testData: Vector[String] = Vector()) extends PuzzleSolver {
 
     val inputData: Vector[String] = if testData.isEmpty then InputReader.read(24) else testData
@@ -13,38 +18,90 @@ class LogicalCircuit(testData: Vector[String] = Vector()) extends PuzzleSolver {
         .map(g => (g.id, g)).toMap
     val outputs: Vector[String] = gates.values.map( _.id ).filter( _.startsWith("z") ).toVector.sorted.reverse
 
-    def calculateCircuitOutput(id: String, input: Map[String, Int]): Int = {
-        if input.contains(id) then
+    def calculateCircuitOutput(id: String, input: Map[String, Int], gates: Map[String, Gate]): Int = {
+        //if input.contains(id) then
+        if id.startsWith("x") || id.startsWith("y") then
             input(id)
         else {
             val gate = gates(id)
-            gate.function(calculateCircuitOutput(gate.input1, input), calculateCircuitOutput(gate.input2, input))
+            gate.function(calculateCircuitOutput(gate.input1, input, gates), calculateCircuitOutput(gate.input2, input, gates))
         }
     }
 
-    def checkOutputForBit(id: String): Boolean = {
+    def checkOutputForBit(id: String, gates: Map[String, Gate]): Boolean = {
         val wire1 = id.replace("z", "x")
         val wire2 = id.replace("z", "y")
-        for (inp1, inp2) <- Set((0, 0), (0, 1), (1, 0), (1, 1))
-            yield inp1 + inp2
-        true
+        val indx = id.replace("z", "").toInt
+        val carryInValues = if indx == 0 then Vector(0) else Vector(0, 1)
+        val (inp1Values, inp2Values) = (Vector(0, 1), Vector(0, 1))
+        (for carryIn <- carryInValues yield
+            for inp1 <- inp1Values yield
+                for inp2 <- inp2Values yield {
+                    val sum = (inp1 ^ inp2) ^ carryIn
+                    val carryOut = (inp1 & inp2) | ((inp1 & inp2) | (carryIn & (inp1 ^ inp2)))
+                    val inputs = Map(
+                        f"x${indx - 1}%02d" -> 1, f"y${indx - 1}%02d" -> carryIn,
+                        f"x$indx%02d" -> inp1, f"y$indx%02d" -> inp2
+                    ).withDefault(_ => 0)
+                    if calculateCircuitOutput(id, inputs, gates) == sum &&
+                        (if indx == outputs.size -1 then true
+                        else calculateCircuitOutput(f"z${indx + 1}%02d", inputs, gates) == carryOut)
+                    then true
+                    else false
+                })
+            .flatten.flatten.forall( _ == true )
+    }
+
+    def getAllGatesForBit(id: String): Vector[String] = {
+        val result = ArrayBuffer[String]()
+        result += id
+        if !inputSignals.contains(id) then {
+            result ++= getAllGatesForBit(gates(id).input1)
+            result ++= getAllGatesForBit(gates(id).input2)
+        }
+        result.sorted.reverse.toVector
+    }
+
+    private def reverseGates(gates: mutable.Map[String, Gate], pairToReverse: List[String]): Unit = {
+        val tempId = "____"
+//        gates.foreach( e => if e._2.input1 == pairToReverse.head then e._2.input1 = tempId)
+//        gates.foreach( e => if e._2.input2 == pairToReverse.head then e._2.input2 = tempId)
+//
+//        gates.foreach( e => if e._2.input1 == pairToReverse.last then e._2.input1 = pairToReverse.head)
+//        gates.foreach( e => if e._2.input2 == pairToReverse.last then e._2.input2 = pairToReverse.head)
+//
+//        gates.foreach( e => if e._2.input1 == tempId then e._2.input1 = pairToReverse.last)
+//        gates.foreach( e => if e._2.input2 == tempId then e._2.input2 = pairToReverse.last)
+    }
+
+    def findReversedPair(tryGates: Set[String], bit: String): List[String] = {
+        boundary:
+            tryGates.toList.combinations(2).foreach( gatesPair =>
+                val thisGates = mutable.Map.from(gates)
+                reverseGates(thisGates, gatesPair)
+                reverseGates(thisGates, (tryGates -- gatesPair.toSet).toList)
+                if checkOutputForBit(bit, thisGates.toMap)
+                    then break(gatesPair)
+            )
+            List()
     }
 
     def printCircuit(output: String, indent: String): Unit = {
-        if inputSignals.contains(output) then println(s"$indent$output")
-        else
+        if !inputSignals.contains(output) then {
             println(s"$indent${gates(output)}")
             printCircuit(gates(output).input1, indent + "  ")
-            printCircuit(gates(output).input2, indent + "  " )
+            printCircuit(gates(output).input2, indent + "  ")
+        }
     }
 
-    def printCircuit(): Unit =
+    def printCircuit(): Unit = {
         for bit <- outputs.reverse do
             printCircuit(bit, "")
             println()
+    }
 
     override def part1: Any =
-        java.lang.Long.parseLong(outputs.map( calculateCircuitOutput(_, inputSignals) ).mkString, 2)
+        java.lang.Long.parseLong(outputs.map( calculateCircuitOutput(_, inputSignals, gates) ).mkString, 2)
 
     override def part2: Any =
         0
@@ -66,89 +123,3 @@ object LogicalCircuit {
             case "XOR" => XOR(out, in1, in2)
     }
 }
-
-/*
-class PulseProcessor(input: List<String>) {
-
-    companion object {
-        const val broadcaster = "broadcaster"
-        private val startPulse = Pulse(PulseType.LOW, "button", broadcaster)
-    }
-    var debug = false
-    var debug2 = false
-    private val aocInputList: List<AoCInput> = InputUtils(AoCInput::class.java).readAoCInput(input)
-    val modules: Map<String,Module> = aocInputList.associate {
-        val id = it.sender.substring(1, it.sender.length)
-        when (it.sender.first()) {
-            'b' -> broadcaster to Broadcaster(it.receivers)
-            '%' -> id to FlipFlop(id, it.receivers)
-            '&' -> id to Conjunction(id, it.receivers)
-            else -> throw AocException("invalid input data [$it]")
-        }
-    }
-    private val endModule = (modules.values.map { it.destinations }.flatten().distinct().toSet()
-            - modules.keys).first()
-    private val endStateInputs = mutableListOf<Pair<Int,Pulse>>()
-    private var cycleCount = 0
-
-    init {
-        updateConjState()
-    }
-
-    fun processPulse(watchModuleId: String = ""): Pair<Int,Int> {
-        val counts = mutableMapOf(PulseType.LOW to 0, PulseType.HIGH to 0, PulseType.NA to 0)
-        counts[startPulse.hl] =  counts[startPulse.hl]?.plus(1)!!
-        val queue = ArrayDeque<Pulse>().also { q -> q.add(startPulse) }
-        while (queue.isNotEmpty()) {
-            val inputPulse = queue.removeFirst()
-            val curModuleId = inputPulse.destination
-            if (curModuleId == watchModuleId)
-                endStateInputs.add(Pair(cycleCount, inputPulse))
-            val outputPulses = modules[curModuleId]?.sendPulses(inputPulse)!!
-            if (debug) println("module $curModuleId received ${inputPulse.hl} from ${inputPulse.sender} - ${modules[curModuleId]}")
-            for (pulse in outputPulses) {
-                counts[pulse.hl] = counts[pulse.hl]?.plus(1)!!
-                if (pulse.destination == endModule)
-                    continue
-                if (pulse.hl != PulseType.NA)
-                    queue.add(pulse)
-            }
-        }
-        return Pair(counts[PulseType.LOW]!!, counts[PulseType.HIGH]!!)
-    }
-
-    fun countPulsesPart1(): Int {
-        var countL = 0
-        var countH = 0
-        repeat(1000) {
-            processPulse().let { countL += it.first; countH += it.second }
-        }
-        return countL * countH
-    }
-
-    fun identifyHighPulseCyclesForFinalConjunction(): Set<Long> {
-        // this is the conjunction module that feeds the final module
-        val moduleToWatch = modules.values.first { it.destinations.contains(endModule) }.id
-        repeat(20000) {
-            cycleCount = it + 1
-            processPulse(moduleToWatch)
-        }
-        val inputCycles = endStateInputs.filter { it.second.hl == PulseType.HIGH }.groupBy { it.second.sender }
-        if (debug2) inputCycles.forEach { it.println() }
-        if (inputCycles.entries.size == (modules[moduleToWatch] as Conjunction).inputs.size &&
-            inputCycles.values.all { it.size >= 3 && it[1].first == it[0].first * 2 && it[2].first == it[0].first * 3 } )
-            return inputCycles.values.map { it[0].first.toLong() }.toSet()
-        throw AocException("could not identify cycle of high signals")
-    }
-
-    private fun updateConjState() {
-        modules.values.filterIsInstance<Conjunction>().forEach { conj ->
-            modules.values.forEach { mod ->
-                if (mod.destinations.contains(conj.id))
-                    conj.inputs[mod.id] = PulseType.LOW
-            }
-        }
-    }
-}
-
- */
--- src/test/scala/day24/TestDay24.scala
+++ src/test/scala/day24/TestDay24.scala
@@ -33,9 +33,9 @@ class TestDay24 extends AnyFlatSpec {
             "x02 OR y02 -> z02"
         )
         val solver = LogicalCircuit(testData)
-        val z00 = solver.calculateCircuitOutput("z00", solver.inputSignals)
-        val z01 = solver.calculateCircuitOutput("z01", solver.inputSignals)
-        val z02 = solver.calculateCircuitOutput("z02", solver.inputSignals)
+        val z00 = solver.calculateCircuitOutput("z00", solver.inputSignals, solver.gates)
+        val z01 = solver.calculateCircuitOutput("z01", solver.inputSignals, solver.gates)
+        val z02 = solver.calculateCircuitOutput("z02", solver.inputSignals, solver.gates)
         println(s"$z02$z01$z00")
         (z02, z01, z00) shouldBe (1, 0, 0)
     }
@@ -46,6 +46,19 @@ class TestDay24 extends AnyFlatSpec {
 
     it should "print diagram of circuit" in {
         solver.printCircuit()
+    }
+
+    it should "verify correct sum and carry for each bit" in {
+        val validBits = solver.outputs.reverse.map( out => solver.checkOutputForBit(out, solver.gates) )
+        validBits.zipWithIndex.foreach(println)
+
+        solver.printCircuit("z05", "")
+        solver.printCircuit("z06", "")
+        val gates5 = solver.getAllGatesForBit("z05")
+        val gates6 = solver.getAllGatesForBit("z06")
+        println(s"additional gates between bits 5 and 6: ${gates6.toSet -- gates5.toSet}")
+
+        println(solver.findReversedPair(Set("png", "dhs", "kbj", "vcv"), "z06"))
     }
 
     it should "solve part2 correctly" in {
